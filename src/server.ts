2e17abdcc3e13f4a4d0d102625cf79389919e9c3
Hasanfx <imrshadow.fx@gmail.com>
2025-05-01 22:42:43 +0300

    aa

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import jsonServer from "json-server";
+import { Application } from "express";
+
+const server: Application = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middleware = jsonServer.defaults();
+const port: number = Number(process.env.PORT) || 4000;
+
+server.use(middleware);
+server.use(router);
+
+
+
+server.listen(port, () => {
+    console.log(`JSON server is running on port ${port}`);
+});
