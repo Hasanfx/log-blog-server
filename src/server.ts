e81160a5126c437565d73a5353f5300065c630ab
Hasanfx <imrshadow.fx@gmail.com>
2025-05-03 20:14:06 +0300

    ss

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import jsonServer from "json-server";
 import { Application } from "express";
+import cors from "cors"
 
 const server: Application = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const middleware = jsonServer.defaults();
 const port: number = Number(process.env.PORT) || 4000;
 
+const corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+server.use(cors(corsOptions));
 server.use(middleware);
 server.use(router);
 
